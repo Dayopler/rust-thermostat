61d33af2c1a31a60ee409bd67724e16f36ddb805
Chat Divin <yanis.gateble@epsi.fr>
2023-05-22 16:29:58 +0200

    Thermostat

--- thermostat/src/main.rs
+++ thermostat/src/main.rs
@@ -1,40 +1,71 @@
+
+
 fn main(){
-    // target value chauffage allumé
-    // value min chauffage allumé
-    // current value
-    // fenetre ouverte ?
-    // mode actif (allumé, eteint, fenetres ouvertes)
-    // si value actuelle, inférieur a value cible && chauffage allumé -> go to value cible
-    // si value actuelle >= value cible && chauffage allumé -> eteindre chauffage jusque value min
 
     println!("Welcome to Thermostat !");
 
-    struct Mode {
-        opened : bool,
-        on: bool,
-        off: bool
+    
+    struct Mode<'a> {
+        opened :  &'a str,
+        on: &'a str,
+        off: &'a  str
+    };
+    
+    let mode:Mode = Mode {
+        opened: "opened",
+        on: "on",
+        off: "off",
+    };
+    
+    // Target value desired by the owner
+    const TARGET_VALUE: f64 = 20.0;
+
+    // Mode of radiators
+    let mut radiatior_mode: &'static str = mode.off;
+
+    // The current value returned by the radiator
+    let mut current_value:f64 = 20.1;
+
+    // State of window
+    let mut opened_window: bool = true;
+    
+    
+    
+    loop {
+
+    if radiatior_mode == mode.on {
+        println!("Radiators ON ! \nValue : {}", current_value);
+
+        if  current_value >= TARGET_VALUE + 0.4 {
+            radiatior_mode = mode.off
+        }
+        current_value += 0.1;
+        wait()
+
+    } else if radiatior_mode == mode.off {
+        println!("Radiators OFF !\nValue : {}", current_value);
+
+        if current_value <= TARGET_VALUE - 0.4 {
+            radiatior_mode = mode.on;
+        } ;
+        current_value -= 0.1;
+
+        wait()
+
+    } else if radiatior_mode == mode.opened {
+        println!("Windows Opened \nValue : {}", current_value);
+
+        radiatior_mode = mode.off;
+
+
+        wait()
     }
 
-    let mut mode = Mode {
-        opened: false,
-        on: false,
-        off: true,
-    };
 
-loop {
-
-    if mode.on {
-        println!("Radiators ON !");
-        wait()
-    } else if mode.off {
-        println!("Radiators OFF !");
-        wait()
-    } else if mode.opened {
-        mode.off = true;
-        mode.on = false;
-        mode.opened = true;
-        println!("Radiators ");
-        wait()
+    // condition if there are window sensors   
+    // don't forget to swith opened_window to false
+    if opened_window {
+        radiatior_mode = mode.opened;
     }
 } 
 
@@ -43,6 +74,5 @@ loop {
 
 fn wait() {
     // Wait 2 seconds
-
     std::thread::sleep(std::time::Duration::from_secs(2));
 }
